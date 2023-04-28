f384358f200cc24dc22862f0713e49608481d96d
Tieelo <tieel@gmx.net>
2023-04-28 10:12:22 +0200

    First Commit with all data up to 28.04.2023

---
+++ src/Generics/M_Box.java
@@ -0,0 +1,17 @@
+package Generics;
+
+public class M_Box<T> {
+    public void setValue(T i) {
+    }
+
+    public T getValue() {
+        return null;
+    }
+
+    public void setMarke(T s) {
+    }
+
+    public T getMarke() {
+        return null;
+    }
+}
