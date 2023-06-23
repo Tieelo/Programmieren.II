c8a5728d7f7ffa003406901494d75c0275921513
Tieelo <tieel@gmx.net>
2023-06-23 10:22:32 +0200

    new files

---
+++ src/designPatterns/singleton/uebung_enum/ChocolateBoiler.java
@@ -0,0 +1,8 @@
+package designPatterns.singleton.uebung_enum;
+
+public class ChocolateBoiler {
+    public static void main(String[] args) {
+
+        ChocolateBoilerStart cb1 = ChocolateBoilerStart.UNIQUE_INSTANCE;
+    }
+}
